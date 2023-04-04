28c30e12180f527515a9ec89a70d2720d04294c6
PepeNavarro7 <navarrocuartero7@gmail.com>
2023-04-04 13:58:57 +0200

    P2 Acabada

--- P2 JavaScript/js/myScript.js
+++ P2 JavaScript/js/myScript.js
@@ -1,21 +1,20 @@
 //Variables globales a las que accederemos
+var abrir=true
+var cajaDesplegable=document.getElementById("desplegable");
 var botonComentarios=document.getElementById("boton-comentario");
 var zonaTextoNuevoComentario=document.getElementById("comentario-nuevo");
-var abrir=true
 var botonSubmit=document.getElementById("submit")
-var seccionSoloComentarios=document.getElementById("desplegable");
 
 //Funciones
 //Funcion que comprueba mediante expresiones regulares si el formato del email es correcto y si no esta vacio
 function comprobarEmail(email){
-    if(email!="" && email.search(/^([0-9a-z\.\_]+)+@{1}([0-9a-z]+\.)+[0-9a-z]+$/i)!=-1)
+    if(email!="" && email.search(/^([0-9a-z\.\_]+)+@{1}([0-9a-z]+\.)+[0-9a-z]+$/i)!=-1) // Esto lo he copiado
         return true
-
     else
         return false
 }
 
-//Funcion que devuelve la fecha actual en formato mas bonito
+//Funcion que devuelve la fecha actual en formato mas legible
 function obtenerFechaActual(){
     var fecha=new Date()
     var mesString;
@@ -24,47 +23,36 @@ function obtenerFechaActual(){
         case 0:
             mesString="enero"
             break;
-
         case 1:
             mesString="febrero"
             break;
-
         case 2:
             mesString="marzo"
             break;
-
         case 3:
             mesString="abril"
             break;          
-            
         case 4:
             mesString="mayo"
             break;
-
         case 5:
             mesString="junio"
             break;
-
         case 6:
             mesString="julio"
             break;
-
         case 7:
             mesString="agosto"
             break;          
-            
         case 8:
             mesString="septiembre"
             break;
-
         case 9:
             mesString="octubre"
             break;
-
         case 10:
             mesString="noviembre"
             break;
-
         case 11:
             mesString="diciembre"
             break;                          
@@ -74,49 +62,17 @@ function obtenerFechaActual(){
     var minutos=(fecha.getMinutes()<10)?"0"+fecha.getMinutes(): fecha.getMinutes();
     var horas=(fecha.getHours()<10)?"0"+fecha.getHours():fecha.getHours();
 
-    return dia+" de "+mesString+" del "+fecha.getFullYear()+", "+horas+":"+minutos;
+    return dia+" de "+mesString+" de "+fecha.getFullYear()+", "+horas+":"+minutos;
 }
 
-function subirOpacidad(elemento){
-    var contador=0
-    var incremento=0.02;
-    var contadorTam=0.8;
-    var incrementoTam=0.005;
-
-    var interval=setInterval(()=>{
-        if(contador<1 || contadorTam<1){
-            if(contador<1){
-                contador+=incremento
-                elemento.style.opacity=contador
-            }
-            
-            if(contadorTam<1){
-                contadorTam+=incrementoTam
-                elemento.style.transform="scale("+contadorTam+")"
-            }
-        }
-        else{
-            clearInterval(interval)
-        }
-
-    }, 24)
-}
-
-//Funciones que se llaman desde eventos
 //Funcion para abrir y cerrar la caja de los comentarios
 function expandirComentarios(){
-    var desplegableComentarios=document.getElementById("comentarios-submit-container");
-
-    seccionSoloComentarios.style.transition="0.5s"  //Se activa la animacion (hay que poner esto debido a que la animacion salta si se cambia el zoom)
-
     if(abrir){
-        seccionSoloComentarios.style.left="0"
-        desplegableComentarios.style.boxShadow="10px 0 10px rgba(0, 0, 0, 0.5)"
+        cajaDesplegable.style.left="0"
         abrir=false;
     }
     else{
-        seccionSoloComentarios.style.left="calc(-1 * min(700px, 65vw))"
-        desplegableComentarios.style.boxShadow=""
+        cajaDesplegable.style.left="calc(-1 * min(600px, 55vw))"
         abrir=true;
     }
 }
@@ -126,17 +82,17 @@ function subirComentario(){
     var nombre=document.getElementById("nombre")
     var email=document.getElementById("email")
     var comentarioNuevo=document.getElementById("comentario-nuevo")
-    var comentarios=document.getElementsByClassName("comentario-container")
-    var template=comentarios[0].cloneNode(true)
 
+    var comentarios=document.getElementsByClassName("comentario-container")
+    // copio la estructura del comentario
+    var template=comentarios[0].cloneNode(true)
 
     if(nombre.value!="" && comprobarEmail(email.value) && comentarioNuevo.value!=""){
         //Se inserta el nuevo comentario justo debajo del final de la caja para crear uno.
-        //Aunque tambien se puede hacer cogiendo todos los comentarios y poniendolo antes de todos en el array
-        comprobarPalabrotas();
-        //IMPORTANTE: InnerHTML sirve para modificar solo lo de dentro del div en este caso, outerHTML es para todo el DOM
+        comprobarPalabrotas(); // Por si la ultima palabra es una palabrota
+        //InnerHTML para crear un nuevo comentario
         template.innerHTML="<div class=\"nombre-comentario\">"+
-                                "<h4>"+nombre.value+"</h4>"+
+                                nombre.value+
                             "</div>"+
                             "<div class=\"comentario-email\">"+
                                 email.value+
@@ -148,13 +104,8 @@ function subirComentario(){
                                 comentarioNuevo.value+
                             "</div>";
 
-        template.style.transform="scale(0.8)"
-        template.style.opacity="0"
-        document.getElementById("comentarios-submit-container").insertBefore(template, comentarios[0])
-        subirOpacidad(template)
-
-        nombre.value=email.value=comentarioNuevo.value=""
-
+        document.getElementById("caja-comentarios").insertBefore(template, comentarios[0])
+        nombre.value=email.value=comentarioNuevo.value="" // reseteo
     }
     else{
         alert("Uno o varios campos están vacíos o son inválidos")
@@ -164,11 +115,7 @@ function subirComentario(){
 //Funcion que comprueba palabrotas de un array de strings
 function comprobarPalabrotas(){
     var texto=document.getElementById("comentario-nuevo");
-    //Version family-friendly
-    //var palabrotas=["palabrota", "imbecil", "terrible", "caca", "patada", "blanco", "pruebame", "fiesta", "malo", "corcholis"]
-
-    //Version realista
-    var palabrotas=["caca", "cerdo", "payaso", "puta", "marrano", "cojon", "polla", "imbecil", "gilipollas", "cabron"]
+    var palabrotas=["palabrota", "imbecil", "terrible", "caca", "patada", "blanco", "pruebame", "fiesta", "malo", "corcholis"]
 
     palabrotas.forEach(
         (aux)=>{       
@@ -177,10 +124,7 @@ function comprobarPalabrotas(){
     )
 }
 
-//Declaracion de eventos
-botonSubmit.addEventListener("click", subirComentario);
+// Los listener
 botonComentarios.addEventListener("click", expandirComentarios);
+botonSubmit.addEventListener("click", subirComentario);
 zonaTextoNuevoComentario.addEventListener("keypress", comprobarPalabrotas)
-seccionSoloComentarios.addEventListener("transitionend", ()=>{
-    seccionSoloComentarios.style.transition="0s";
-})
